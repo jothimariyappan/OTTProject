e24bd5a5c2c1f5702fb890eff081b9b3b6bcb88e
JO <jothimariyappan6@gmail.com>
2023-01-27 20:10:39 +0530

    Initial Commit

---
+++ OTTProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTTProject
+//
+//  Created by Jo on 27/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
